df79acda0669032af4f5aa567622e038242cc67f
Paulo Barros <paulo.alfredo.barros@gmail.com>
2024-12-27 22:55:43 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html className="light" lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
