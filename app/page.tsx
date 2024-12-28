af542caca8c1e8b83bf478a159f47c3a088a3643
pabrrs <paulo.alfredo.barros@gmail.com>
2024-12-27 20:32:15 -0400

    fix(pixels): adjust wrap

--- app/page.tsx
+++ app/page.tsx
@@ -13,53 +13,44 @@ function PixelBrought({ link, logo, headline }: { link: string, logo: string, he
 
 function PixelEmpty() {
   return (
-    <div className="h-5 w-5 bg-slate-200 border-dotted border-2 border-slate-300 rounded-sm">
-    </div>
+    <div className="h-5 w-5 bg-slate-200 border-dotted border-2 border-slate-300 rounded-sm" />
   )
 }
 
 export default function Home() {
 
-  const rows = Array.from(Array(100).keys()).map((row, rowIndex) => {
-    const cols = Array.from(Array(100).keys()).map((col, colIndex) => {
-      const brand = BRANDS.find(coItem => coItem.pixels.includes(row + col))
-        if (brand) {
-          return <PixelBrought key={colIndex} {...brand} />
-        } else {
-          return <PixelEmpty key={colIndex} />
-        }
-    })
-    return (
-      <div key={rowIndex} className="flex flex-row gap-1 items-center">
-        {cols}
-      </div>
-    )
+  const rows = Array.from(Array(100000).keys()).map((row, rowIndex) => {
+    const brand = BRANDS.find(coItem => coItem.pixels.includes(row))
+      if (brand) {
+        return <PixelBrought key={rowIndex} {...brand} />
+      } else {
+        return <PixelEmpty key={rowIndex} />
+      }
   })
-  
 
   return (
     <main className="mx-20 mb-10 min-h-screen">
-    <header className="flex flex-row justify-between py-10">
-      <div>
-        <Image src="logo.png" width={200} height={100} alt="um milhão de pixels"/>
+      <header className="flex flex-row justify-between py-10 items-center">
+        <div>
+          <Image src="logo.png" width={200} height={100} alt="um milhão de pixels"/>
+        </div>
+        <div className="text-center">
+          <h1 className="text-lg font-bold text-slate-800 uppercase">
+            A vitrine publicitária mais barata do Brasil 🇧🇷
+          </h1>
+          <h2 className="text-sm font-thin">
+            Anuncie sua marca por apenas R$ 1 💰
+          </h2>
+        </div>
+        <div>
+          <a href="/tst" className="bg-slate-900 text-white font-bold text-sm px-6 py-4 rounded-lg shadow-md hover:bg-slate-700">
+            Quero Anunciar
+          </a>
+        </div>
+      </header>
+      <div className="flex gap-2" style={{flexWrap: "wrap"}}>
+        {rows}
       </div>
-      <div className="text-center">
-        <h1 className="text-lg font-bold text-slate-600 uppercase">
-          A vitrine publicitária mais barata do Brasil 🇧🇷
-        </h1>
-        <h2 className="text-sm font-thin">
-          Anuncie sua marca por apenas R$ 1 💰
-        </h2>
-      </div>
-      <div>
-        <a href="/tst" className="bg-slate-900 text-white font-bold text-sm px-6 py-4 rounded-lg shadow-md hover:bg-slate-700">
-          Quero Anunciar
-        </a>
-      </div>
-    </header>
-    <div className="flex flex-col gap-2">
-      {rows}
-    </div>
     </main>
   );
 }
