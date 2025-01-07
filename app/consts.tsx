572f0f645882056f21bc33ee512c44144b679eb3
pabrrs <paulo.alfredo.barros@gmail.com>
2025-01-07 18:20:56 -0400

    feat: add dvstt and oly

--- app/consts.tsx
+++ app/consts.tsx
@@ -11,4 +11,18 @@ export const BRANDS = [{
     headline: "Coma mais frango",
     link: "https://www.instagram.com/galeteriakifrango/",
     pixels: [1]
+},
+{
+    name: "DVSTT",
+    logo: "/brands/dvstt.png",
+    headline: "DJ & Produtor",
+    link: "https://instagram.com/dvsttoficial/",
+    pixels: [2]
+},
+{
+    name: "Oly",
+    logo: "/brands/oly.jpeg",
+    headline: "Feita para a deusa que habita em você 🪽",
+    link: "https://www.instagram.com/oly.modaintima/",
+    pixels: [3]
 }]
