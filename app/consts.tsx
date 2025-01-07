8d006695c88366ba5f07fd285fa5bea22a4d5724
pabrrs <paulo.alfredo.barros@gmail.com>
2025-01-07 17:56:13 -0400

    feat: add kifrango

--- app/consts.tsx
+++ app/consts.tsx
@@ -4,4 +4,11 @@ export const BRANDS = [{
     headline: "Você no controle da sua carreira",
     link: "https://elevatte.me",
     pixels: [0]
+},
+{
+    name: "Ki Frango",
+    logo: "/brands/kifrango.png",
+    headline: "Coma mais frango",
+    link: "https://www.instagram.com/galeteriakifrango/",
+    pixels: [1]
 }]
