939d0abd0b0c4fc946ed2d55003e7d6c06622c91
pabrrs <paulo.alfredo.barros@gmail.com>
2025-01-08 15:49:12 -0400

    feat: new pixels

--- app/consts.tsx
+++ app/consts.tsx
@@ -1,72 +1,114 @@
-export const BRANDS = [{
-    name: "Elevatte",
-    logo: "/brands/elevatte.png",
-    headline: "Você no controle da sua carreira",
-    link: "https://elevatte.me",
-    pixels: [0]
-},
-{
-    name: "Ki Frango",
-    logo: "/brands/kifrango.png",
-    headline: "Coma mais frango",
-    link: "https://www.instagram.com/galeteriakifrango/",
-    pixels: [1]
-},
-{
-    name: "DVSTT",
-    logo: "/brands/dvstt.png",
-    headline: "DJ & Produtor",
-    link: "https://instagram.com/dvsttoficial/",
-    pixels: [2]
-},
-{
-    name: "Oly",
-    logo: "/brands/oly.jpeg",
-    headline: "Feita para a deusa que habita em você 🪽",
-    link: "https://www.instagram.com/oly.modaintima/",
-    pixels: [3]
-},
-{
-    name: "Neograde",
-    logo: "/brands/neograde.jpg",
-    headline: "Sites, E-commerces e Aplicativos",
-    link: "https://www.instagram.com/neograde.tech/",
-    pixels: [4]
-},
-{
-    name: "Cabana criativa",
-    logo: "/brands/cabana-criativa.png",
-    headline: "Presentear é dar forma ao amor",
-    link: "https://www.instagram.com/cabana_criativa/",
-    pixels: [5]
-},{
-    name: "Croc Artesanato",
-    logo: "/brands/croc.jpg",
-    headline: "Artesanato em crochê, feitos por encomenda e algumas coisinhas a pronta entrega 🥰",
-    link: "https://www.instagram.com/croc.artesanato/",
-    pixels: [6]
-},{
-    name: "VIRALATA PRODUÇÕES",
-    logo: "/brands/viralata-prod.png",
-    headline: "🐶 festa de música eletrônica diferenciada 🐶",
-    link: "https://www.instagram.com/viralataprod/",
-    pixels: [7]
-},{
-    name: "viniREMELA",
-    logo: "/brands/viniremela.png",
-    headline: "o astro do momento",
-    link: "https://www.instagram.com/viniremela/",
-    pixels: [8]
-},{
-    name: "Mimo",
-    logo: "/brands/mimo.png",
-    headline: "⚠️ este gato não está à venda, apenas para fins de apreciação de sua beleza!",
-    link: "https://www.instagram.com/belotibordado/",
-    pixels: [9]
-},{
-    name: "belo(ti)bordado",
-    logo: "/brands/belotibordado.png",
-    headline: "dou vida em linha e bordado para personagens geek e outras coisas ;p",
-    link: "https://www.instagram.com/belotibordado/",
-    pixels: [10]
-}]
+export const BRANDS = [
+    {
+        name: "Elevatte",
+        logo: "/brands/elevatte.png",
+        headline: "Você no controle da sua carreira",
+        link: "https://elevatte.me",
+        pixels: [0]
+    },
+    {
+        name: "Ki Frango",
+        logo: "/brands/kifrango.png",
+        headline: "Coma mais frango",
+        link: "https://www.instagram.com/galeteriakifrango/",
+        pixels: [1]
+    },
+    {
+        name: "DVSTT",
+        logo: "/brands/dvstt.png",
+        headline: "DJ & Produtor",
+        link: "https://instagram.com/dvsttoficial/",
+        pixels: [2]
+    },
+    {
+        name: "Oly",
+        logo: "/brands/oly.jpeg",
+        headline: "Feita para a deusa que habita em você 🪽",
+        link: "https://www.instagram.com/oly.modaintima/",
+        pixels: [3]
+    },
+    {
+        name: "Neograde",
+        logo: "/brands/neograde.jpg",
+        headline: "Sites, E-commerces e Aplicativos",
+        link: "https://www.instagram.com/neograde.tech/",
+        pixels: [4]
+    },
+    {
+        name: "Cabana criativa",
+        logo: "/brands/cabana-criativa.png",
+        headline: "Presentear é dar forma ao amor",
+        link: "https://www.instagram.com/cabana_criativa/",
+        pixels: [5]
+    },
+    {
+        name: "Croc Artesanato",
+        logo: "/brands/croc.jpg",
+        headline: "Artesanato em crochê, feitos por encomenda e algumas coisinhas a pronta entrega 🥰",
+        link: "https://www.instagram.com/croc.artesanato/",
+        pixels: [6]
+    },
+    {
+        name: "VIRALATA PRODUÇÕES",
+        logo: "/brands/viralata-prod.png",
+        headline: "🐶 festa de música eletrônica diferenciada 🐶",
+        link: "https://www.instagram.com/viralataprod/",
+        pixels: [7]
+    },
+    {
+        name: "viniREMELA",
+        logo: "/brands/viniremela.png",
+        headline: "o astro do momento",
+        link: "https://www.instagram.com/viniremela/",
+        pixels: [8]
+    },
+    {
+        name: "Mimo",
+        logo: "/brands/mimo.png",
+        headline: "⚠️ este gato não está à venda, apenas para fins de apreciação de sua beleza!",
+        link: "https://www.instagram.com/belotibordado/",
+        pixels: [9]
+    },
+    {
+        name: "belo(ti)bordado",
+        logo: "/brands/belotibordado.png",
+        headline: "dou vida em linha e bordado para personagens geek e outras coisas ;p",
+        link: "https://www.instagram.com/belotibordado/",
+        pixels: [10]
+    },
+    {
+        name: "EgoBrain",
+        logo: "/brands/egobrain.png",
+        headline: "11/01/25, Galpão do Rock, Insurgents motoclube - Rua Maria Naymaier, 4985",
+        link: "https://www.sympla.com.br/evento/rock-paulera-o-lado-troo-da-vida/2777795?share_id=whatsapp",
+        pixels: [11]
+    },
+    {
+        name: "Gaudério News",
+        logo: "/brands/gauderionews.png",
+        headline: "Notícias do Noroeste Gaúcho",
+        link: "https://gauderionews.com.br/",
+        pixels: [12]
+    },
+    {
+        name: "Madeirão",
+        logo: "/brands/madeirao.png",
+        headline: "Informação é com a gente!",
+        link: "https://madeiraoweb.com.br/",
+        pixels: [13]
+    },
+    {
+        name: "O Bandeirante",
+        logo: "/brands/bandeirante.png",
+        headline: "Jornal do distrito de União Bandeirantes, Porto Velho, Rondônia",
+        link: "https://obandeirantenews.com.br",
+        pixels: [14]
+    },
+    {
+        name: "BH Ao Vivo",
+        logo: "/brands/bhaovivo.png",
+        headline: "Seu resumo diário de notícias",
+        link: "https://bhaovivo.com.br/",
+        pixels: [15]
+    }
+]
