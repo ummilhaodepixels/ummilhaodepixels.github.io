0f36a1512a49adbfb67c6eb7b4d5a395791ba39f
Rafael Ricci <rlopes.ricci@hotmail.com>
2025-01-08 23:11:33 -0400

    refactor(images): replace jpg jpeg png by webp

--- app/consts.tsx
+++ app/consts.tsx
@@ -1,161 +1,161 @@
 export const BRANDS = [
     {
         name: "Elevatte",
-        logo: "/brands/elevatte.png",
+        logo: "/brands/elevatte.webp",
         headline: "Você no controle da sua carreira",
         link: "https://elevatte.me",
         pixels: [0]
     },
     {
         name: "Ki Frango",
-        logo: "/brands/kifrango.png",
+        logo: "/brands/kifrango.webp",
         headline: "Coma mais frango",
         link: "https://www.instagram.com/galeteriakifrango/",
         pixels: [1]
     },
     {
         name: "DVSTT",
-        logo: "/brands/dvstt.png",
+        logo: "/brands/dvstt.webp",
         headline: "DJ & Produtor",
         link: "https://instagram.com/dvsttoficial/",
         pixels: [2]
     },
     {
         name: "Oly",
-        logo: "/brands/oly.jpeg",
+        logo: "/brands/oly.webp",
         headline: "Feita para a deusa que habita em você 🪽",
         link: "https://www.instagram.com/oly.modaintima/",
         pixels: [3]
     },
     {
         name: "Neograde",
-        logo: "/brands/neograde.jpg",
+        logo: "/brands/neograde.webp",
         headline: "Sites, E-commerces e Aplicativos",
         link: "https://www.instagram.com/neograde.tech/",
         pixels: [4]
     },
     {
         name: "Cabana criativa",
-        logo: "/brands/cabana-criativa.png",
+        logo: "/brands/cabana-criativa.webp",
         headline: "Presentear é dar forma ao amor",
         link: "https://www.instagram.com/cabana_criativa/",
         pixels: [5]
     },
     {
         name: "Croc Artesanato",
-        logo: "/brands/croc.jpg",
+        logo: "/brands/croc.webp",
         headline: "Artesanato em crochê, feitos por encomenda e algumas coisinhas a pronta entrega 🥰",
         link: "https://www.instagram.com/croc.artesanato/",
         pixels: [6]
     },
     {
         name: "VIRALATA PRODUÇÕES",
-        logo: "/brands/viralata-prod.png",
+        logo: "/brands/viralata-prod.webp",
         headline: "🐶 festa de música eletrônica diferenciada 🐶",
         link: "https://www.instagram.com/viralataprod/",
         pixels: [7]
     },
     {
         name: "viniREMELA",
-        logo: "/brands/viniremela.png",
+        logo: "/brands/viniremela.webp",
         headline: "o astro do momento",
         link: "https://www.instagram.com/viniremela/",
         pixels: [8]
     },
     {
         name: "Mimo",
-        logo: "/brands/mimo.png",
+        logo: "/brands/mimo.webp",
         headline: "⚠️ este gato não está à venda, apenas para fins de apreciação de sua beleza!",
         link: "https://www.instagram.com/belotibordado/",
         pixels: [9]
     },
     {
         name: "belo(ti)bordado",
-        logo: "/brands/belotibordado.png",
+        logo: "/brands/belotibordado.webp",
         headline: "dou vida em linha e bordado para personagens geek e outras coisas ;p",
         link: "https://www.instagram.com/belotibordado/",
         pixels: [10]
     },
     {
         name: "EgoBrain",
-        logo: "/brands/egobrain.png",
+        logo: "/brands/egobrain.webp",
         headline: "11/01/25, Galpão do Rock, Insurgents motoclube - Rua Maria Naymaier, 4985",
         link: "https://www.sympla.com.br/evento/rock-paulera-o-lado-troo-da-vida/2777795?share_id=whatsapp",
         pixels: [11]
     },
     {
         name: "Gaudério News",
-        logo: "/brands/gauderionews.png",
+        logo: "/brands/gauderionews.webp",
         headline: "Notícias do Noroeste Gaúcho",
         link: "https://gauderionews.com.br/",
         pixels: [12]
     },
     {
         name: "Madeirão",
-        logo: "/brands/madeirao.png",
+        logo: "/brands/madeirao.webp",
         headline: "Informação é com a gente!",
         link: "https://madeiraoweb.com.br/",
         pixels: [13]
     },
     {
         name: "O Bandeirante",
-        logo: "/brands/bandeirante.png",
+        logo: "/brands/bandeirante.webp",
         headline: "Jornal do distrito de União Bandeirantes, Porto Velho, Rondônia",
         link: "https://obandeirantenews.com.br",
         pixels: [14]
     },
     {
         name: "BH Ao Vivo",
-        logo: "/brands/bhaovivo.png",
+        logo: "/brands/bhaovivo.webp",
         headline: "Seu resumo diário de notícias",
         link: "https://bhaovivo.com.br/",
         pixels: [15]
     },
     {
         name: "Centro de Treinamento Instituo Maria Auxiliadora (CT IMA)",
-        logo: "/brands/ctima.jpeg",
+        logo: "/brands/ctima.webp",
         headline: "Esporte & qualidade de vida",
         link: "https://www.instagram.com/ct.imapvh?igsh=MWpjZjFoMW9iN2wyZg==",
         pixels: [16]
     },
     {
         name: "Central F.C.",
-        logo: "/brands/centralfc.png",
+        logo: "/brands/centralfc.webp",
         headline: "Central Desportivo: Amantes do esporte",
         link: "https://www.instagram.com/central.fc.pvh?igsh=bGg0Z3p3a3V6em9k",
         pixels: [17]
     },
     {
         name: "Eco+",
-        logo: "/brands/eco.jpg",
+        logo: "/brands/eco.webp",
         headline: "Soluções Ambientais",
         link: "http://wa.me/5569984483035",
         pixels: [18]
     },
     {
         name: "Luar Espaço Infantil",
-        logo: "/brands/luarespacoinfantil.png",
+        logo: "/brands/luarespacoinfantil.webp",
         headline: "Terapia Ocupacional",
         link: "https://www.instagram.com/luar.espacoinfantil?igsh=MWNlb2F3eWFmdTk0cA==",
         pixels: [19]
     },
     {
         name: "DL Consultoria & Consórcios",
-        logo: "/brands/dlconsultoria.jpeg",
+        logo: "/brands/dlconsultoria.webp",
         headline: "Promotora de créditos",
         link: "https://www.instagram.com/dlconsultoriaoficial?igsh=MWZybzdmY3JtZTRzYg%3D%3D&utm_source=qr",
         pixels: [20]
     },
     {
         name: "itsabiker",
-        logo: "/brands/itsabiker.jpeg",
+        logo: "/brands/itsabiker.webp",
         headline: "Ex-sedentario, computeiro, nortista e ciclista entusiasta",
         link: "https://www.youtube.com/@itsabiker",
         pixels: [21]
     },
     {
         name: "Marcelo Régis | In Foco",
-        logo: "/brands/marceloregis.png",
+        logo: "/brands/marceloregis.webp",
         headline: "Ininterrupto, desde 1999.",
         link: "https://www.instagram.com/marceloregisoficial/",
         pixels: [22]
